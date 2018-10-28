02e426fbe889f7c9b1149ff70fdaac8b2544d9cf
AOQNRMGYXLMV <489365227@qq.com>
2018-10-29 03:44:30 +0800

    add drawSurface

--- src/components/RubicCanvas.tsx
+++ src/components/RubicCanvas.tsx
@@ -10,7 +10,10 @@ interface Point {
     y: number;
 }
 
+type Vectors = Point[];
+
 export class RubicCanvas extends React.Component<Props, object> {
+    private borderLength: number;
     private canvasRef: React.RefObject<HTMLCanvasElement>;
     private ctx: CanvasRenderingContext2D | null;
 
@@ -31,28 +34,65 @@ export class RubicCanvas extends React.Component<Props, object> {
 
     }
 
-    drawCubeBorder = (borderLength: number = 200): void => {
+    drawCubeBorder = (borderLength: number = 250): void => {
+        this.borderLength = borderLength;
         if (this.ctx) {
             this.ctx.save();
 
             const sqrt3 = Math.sqrt(3);
             const center: Point = {
-                x: 200,
+                x: 250,
                 y: 200
             };
             const diamondWidth = borderLength * sqrt3;
             const diamondHeight = borderLength;
-            this.drawDiamond(center, diamondWidth, diamondHeight);
+            let vectors: Vectors = [{
+                x: sqrt3 / 2,
+                y: -1 / 2,
+            }, {
+                x: sqrt3 / 2,
+                y: 1 / 2,
+            }];
+            this.drawSurface(center, vectors, diamondWidth, diamondHeight);
 
             center.x -= borderLength * sqrt3 / 4;
             center.y += borderLength * 3 / 4;
-            this.drawDiamond(center, diamondWidth, diamondHeight, 60);
+            vectors = [{
+                x: sqrt3 / 2,
+                y: 1 / 2,
+            }, {
+                x: 0,
+                y: 1,
+            }]
+            this.drawSurface(center, vectors, diamondWidth, diamondHeight, 60);
 
             center.x += borderLength * sqrt3 / 2;
-            this.drawDiamond(center, diamondWidth, diamondHeight, -60);
+            vectors = [{
+                x: sqrt3 / 2,
+                y: -1 / 2,
+            }, {
+                x: 0,
+                y: 1
+            }]
+            this.drawSurface(center, vectors, diamondWidth, diamondHeight, -60);
 
             this.ctx.restore();
         }
+    }
+
+    drawSurface(center: Point, vectors: Vectors, width: number, height: number, rotateAngle: number = 0): void {
+        const gridWidth = width / 3;
+        const gridHeight = height / 3;
+        const gridBorderLength = this.borderLength / 3;
+        for (let i = -1; i <= 1; i++) {
+            for (let j = -1; j <= 1; j++) {
+                const gridCenter: Point = {
+                    x: center.x + vectors[0].x * i * gridBorderLength + vectors[1].x * j * gridBorderLength,
+                    y: center.y + vectors[0].y * i * gridBorderLength + vectors[1].y * j * gridBorderLength,
+                };
+                this.drawDiamond(gridCenter, gridWidth, gridHeight, rotateAngle);
+            }
+        }        
     }
 
     drawDiamond = (center: Point, width: number, height: number, rotateAngle: number = 0): void => {
@@ -60,13 +100,12 @@ export class RubicCanvas extends React.Component<Props, object> {
             this.ctx.save();
 
             this.ctx.strokeStyle = '#000000';
-            this.ctx.lineWidth = 5;
+            this.ctx.lineWidth = 4;
             this.ctx.lineJoin = 'round';
             this.ctx.translate(center.x, center.y);
             this.ctx.rotate(rotateAngle * Math.PI / 180);
+
             this.ctx.beginPath();
-            // this.ctx.fillStyle = '#FF0000';
-            // this.ctx.fillRect(0, 0, 10, 10);
             this.ctx.moveTo(-width / 2, 0);
             this.ctx.lineTo(0, -height / 2);
             this.ctx.lineTo(width / 2, 0);
